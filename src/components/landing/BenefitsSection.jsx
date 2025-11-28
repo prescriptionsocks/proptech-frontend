c6908960781561059308c5943d8cd9597dae02ed
Sam Baraka <barakaarunga@gmail.com>
2025-11-29 00:40:40 +0300

    added bebefits/features section + styling

--- src/components/landing/BenefitsSection.jsx
+++ src/components/landing/BenefitsSection.jsx
@@ -1,16 +1,26 @@
 import { Link } from "react-router-dom";
 import "./BenefitsSection.css";
+import '../../App.css'
 
 export default function BenefitsSection({ content }) {
   return (
     <section className="section">
       <div className="section-padding">
         <div className="benefits-card-component">
-          <ul>
+             <div className="heading-wrapper">
+                <p className="heading-eyebrow"> Our Features</p>
+                <h2> See what sets up apart</h2>
+
+
+
+            </div>
+          <ul className="benefits-card-list">
+           
             {content.map((feature) => {
               return (
                 <li className="benefits-card">
                   <h3>{feature.title}</h3>
+                  <img className="benefits-card-bg" src={feature.bg} alt="" />
                 </li>
               );
             })}
