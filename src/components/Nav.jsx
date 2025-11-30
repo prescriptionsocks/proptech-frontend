1db4c91e573fc0b068aba09febdf5b00f0c44db8
Sam Baraka <barakaarunga@gmail.com>
2025-11-30 03:16:15 +0300

    added nav from Kerubo + styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,22 +1,22 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import "./Nav.css";
 
 export default function Nav() {
   return (
-    <nav>
-      <div className="secion-padding">
+    <nav className="nav">
+      <div className="section-padding is-nav">
         <div className="nav-flex">
           <Link to="/" className="nav-logo">
-      
-      </Link> {" "}
-      
+          PROPTECH
+          </Link>
 
-      <Link to="/">Home</Link> |{" "}
-      <Link to="/add">Add Property</Link> |{" "}
-      <Link to="/about">About</Link>
-
+          <div className="nav-list">
+            <Link className="nav-lin" to="/">Home</Link>
+            <Link className="nav-lin" to="/add">Add Property</Link>
+            <Link className="nav-lin" to="/about">About</Link>
+          </div>
         </div>
       </div>
-      
     </nav>
-  )
+  );
 }
