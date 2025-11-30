d0a5d604e1f4f1a39ff661293d3d4d7bde94fc78
Sam Baraka <barakaarunga@gmail.com>
2025-11-30 05:05:20 +0300

    added property details page header and text

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -51,7 +51,7 @@ export default function PropertyCard({ house }) {
           </div>
         </div>
       </div>
-      <Link to='/' className="property-card-link">
+      <Link to={`/pages/${house.id}`} className="property-card-link">
       <svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M1.41 0L0 1.41L4.58 6L0 10.59L1.41 12L7.41 6L1.41 0Z" fill="black"/>
 </svg>
--- src/pages/PropertyDetails.jsx
+++ src/pages/PropertyDetails.jsx
@@ -1,4 +1,6 @@
 import { useParams } from "react-router-dom"
+import PropertyCard from "../components/PropertyCard";
+import "./PropertyDetails.css"
 
 export default function PropertyDetails({houses, loading }) {
   
@@ -6,16 +8,36 @@ export default function PropertyDetails({houses, loading }) {
   // get ide from clicked link
   const { id } = useParams()
 
+    if (loading) return <p>Loading...</p>;
+
   //only show data related to the id
 const property = houses.find(h => Number(h.id) === Number(id));
 
 
   return <>
-  <h1>{property.title}</h1>
-  <p>{property.rent}</p>
-  <p>{property.city}</p>
-  <p>{property.estate}</p>
-  <p>{property.description}</p>
+  <main>
+    <section>
+      <div className="section-padding">
+        <div className="details-page-flex">
+          <PropertyCard house={property}/>
+          <div className="details-features-wrapper">
+            <p className="details-feature">{property.bedrooms}</p>
+            <p className="details-feature">{property.bedrooms}</p>
+            <p className="details-feature">{property.bedrooms}</p>
+            <p className="details-feature">{property.bedrooms}</p>
+
+          </div>
+          <div className="details-text-wrapper">
+            <p>{property.description}</p>
+
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+    
+  </main>
   
   </> 
 }
