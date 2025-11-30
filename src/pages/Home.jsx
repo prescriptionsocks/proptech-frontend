36acf201bdee8dc52a50a5f0ca695a1a1af8e837
Sam Baraka <barakaarunga@gmail.com>
2025-11-30 04:00:04 +0300

    added search input

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,40 @@
-import { Link } from "react-router-dom"
-import PropertyList from "../components/PropertyList"
-export default function Home({loading, houses}) {
-  
-  if (loading) {return <p>Loading...</p>}
-  return <>
-  <main>
-<PropertyList houses={houses}/>
-  </main>
-  
-  
-  
-  </>
+import { Link } from "react-router-dom";
+import PropertyList from "../components/PropertyList";
+import { useState } from "react";
+export default function Home({ loading, houses }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  //filter by search
+  const filteredHouses = houses.filter((house) => {
+    const term = searchTerm.toLocaleLowerCase();
+    return (
+      house.title.toLowerCase().includes(term) ||
+      house.city.toLowerCase().includes(term) ||
+      house.estate.toLowerCase().includes(term)
+    );
+  });
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <>
+      <main style={{ marginTop: "124px" }}>
+        <section>
+          <div className="section-padding" style={{ paddingBottom: "0" }}>
+            <div className="search-bar">
+              <input
+                type="text"
+                placeholder="Search by title, city or estate"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+              />
+            </div>
+          </div>
+        </section>
+
+        <PropertyList houses={filteredHouses} />
+      </main>
+    </>
+  );
 }
